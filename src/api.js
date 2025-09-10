4680d750af5fb18f74cb5587ac7cee72013f8cb0
DevSmd24 <shafismd.dev@gmail.com>
2025-09-10 13:12:55 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api", // backend base URL
-  baseURL: "https://dummy-backend-i0bs.onrender.com/",
+  baseURL: "https://dummy-backend-i0bs.onrender.com/api",
 });
 
 export default api;
